9ac5c646345814c75bcea79e049a5aa959bffb18
edyatl <edyatl@yandex.ru>
2025-08-03 21:01:28 +0300

    Fix werkzeug.routing.exceptions.BuildError: Could not build url for endpoint 'index'.

--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 from app import app
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, redirect, url_for
 from flask_security import auth_required, roles_accepted, \
         current_user
 
@@ -23,9 +23,14 @@ def registered():
     return render_template('index.html', text=res)
 
 
-@app.route('/')
-@auth_required()
-@roles_accepted("admin", "editor")
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# @auth_required()
+# @roles_accepted("admin", "editor")
+# def index():
+    # return render_template('index.html')
 
+@app.route('/')
+def index():
+    if current_user.is_authenticated and (current_user.has_role('admin') or current_user.has_role('editor')):
+        return render_template('index.html')
+    return redirect(url_for('security.login'))
